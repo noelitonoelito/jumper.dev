c49a7b4095eb7560ef12bb20de14e2f7585b5bf7
Jeffrey Cardona <noelitonoelito@gmail.com>
2021-05-15 22:11:45 -0400

    ...

--- website/serviceWorker.js
+++ website/serviceWorker.js
@@ -3,7 +3,7 @@
 const APP_PREFIX = "jumper.dev"
 // version of the off-line cache.
 //   change this value every time you want to update cache.
-const VERSION = "v0.22.0"
+const VERSION = "v0.22.1"
 const CACHE_NAME = `${APP_PREFIX}_${VERSION}`
 const URLS_TO_CACHE = [
   // use "/" instead of "/index.html"
@@ -66,21 +66,102 @@ function deleteExpiredCache(event) {
   )
 }
 
+// function fromCacheElseFetch(event) {
+//   event.respondWith(
+//     caches.match(event.request).then((cachedResponse) => {
+//       if (cachedResponse) {
+//         console.log("responding with cache:", event.request.url)
+//         return cachedResponse
+//       }
+
+//       console.log("file is not cached, fetching:", event.request.url)
+//       return fetch(event.request)
+
+//       // return request || fetch(e.request)
+//     })
+//   )
+// }
+
+
 function fromCacheElseFetch(event) {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        console.log("responding with cache:", event.request.url)
-        return cachedResponse
-      }
+  console.log("Handling fetch event for", event.request.url)
 
-      console.log("file is not cached, fetching:", event.request.url)
-      return fetch(event.request)
+  if (event.request.headers.get("range")) {
+    var pos =
+      Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get("range"))[1])
+    console.log(
+      "Range request for",
+      event.request.url,
+      ", starting position:",
+      pos
+    )
 
-      // return request || fetch(e.request)
-    })
-  )
+    event.respondWith(
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.match(event.request.url))
+        .then(async (res) => {
+          if (res) { return res.arrayBuffer() }
+          return fetch(event.request).then((res) => res.arrayBuffer())
+        })
+        .then((ab) => {
+          return new Response(
+            ab.slice(pos),
+            {
+              status: 206,
+              statusText: "Partial Content",
+              headers: [
+                // ["Content-Type", "video/webm"],
+                [
+                  "Content-Range", "bytes " + pos + "-" +
+                  (ab.byteLength - 1) + "/" + ab.byteLength
+                ]
+              ]
+            }
+          )
+        })
+    )
+  } else {
+    console.log("Non-range request for", event.request.url)
+
+    event.respondWith(
+      // caches.match() will look for a cache entry in all of the caches
+      //   available to the service worker.
+      // It's an alternative to first opening a specific named cache
+      //   and then matching on that.
+      caches.match(event.request).then(function (response) {
+        if (response) {
+          console.log("Found response in cache:", response)
+          return response
+        }
+
+        console.log(
+          "No response found in cache. About to fetch from network..."
+        )
+
+        // event.request will always have the proper mode set
+        //   ("cors", "no-cors", etc.) so we don't have to hard code
+        //   "no-cors" like we do when fetch()ing in the install handler.
+        return fetch(event.request).then((response) => {
+          console.log("Response from network is:", response)
+
+          return response
+        })
+          .catch((error) => {
+            // This catch() will handle exceptions thrown from the fetch()
+            //   operation.
+            // Note that a HTTP error response (e.g. 404) will NOT trigger
+            //   an exception.
+            // It will return a normal response object that has the
+            //   appropriate error code set.
+            console.error('Fetching failed:', error)
+
+            throw error
+          })
+      })
+    )
+  }
 }
+
 
 self.addEventListener("install", cacheResources)
 self.addEventListener("activate", deleteExpiredCache)
